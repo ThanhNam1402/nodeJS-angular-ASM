5548d0104b5ea8691f5d503e7015234951085290
ThanhNam <thanhnam4012@gmail.com>
2024-06-08 16:54:17 +0700

    Api crud user

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,33 +1,23 @@
 var cors = require('cors')
 
-// nhung domain duoc phep truy cap tai nguyen server 
-// client and creater 
-const WHITELIST_DOMAINS = ['http://localhost:3000', 'http://localhost:3004', 'http://localhost:5173']
-
-
+const WHITELIST_DOMAINS = ['http://localhost:3000', 'http://localhost:3004', 'http://localhost:5173', 'http://localhost:4200']
 
 // cau hinh corsOption 
 export const corsOptions = {
     origin: function (origin, callback) {
-        // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-        // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
         if (!origin) {
             // && env.BUILD_MODE === 'dev'
             return callback(null, true)
         }
 
-        // Kiểm tra dem origin có phải là domain được chấp nhận hay không
         if (WHITELIST_DOMAINS.includes(origin)) {
             return callback(null, true)
         }
 
-        // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
         return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
     },
 
     optionsSuccessStatus: 200,
-
-    // CORS sẽ cho phép nhận cookies từ request
     credentials: true
 }
 
--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -8,11 +8,9 @@ import userController from "../controllers/systemController/userController";
 
 let router = express.Router();
 
-
-// // draft
 router.get('/users', userController.getAllUsers);
-router.get('users/:id', userController.getOneUser);
-router.post('/addUser', userController.addUser);
+router.get('/users/:id', userController.getOneUser);
+router.post('/users/new', userController.addUser);
 router.put('/users/:id', userController.updateUser);
 router.delete('/users/:id', userController.delUser);
 
